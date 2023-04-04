ac3962c840817108a02161ad268283bc9ae9fd18
Maria Matviian <matviyanm@gmail.com>
2023-04-04 14:53:06 -0400

    Use the hook for Retrieving User Location

--- hooks/use-track-location.js
+++ hooks/use-track-location.js
@@ -3,28 +3,38 @@ import React, { useState } from "react";
 const useTrackLocation = () => {
   const [locationErrorMsg, setLocationErrorMsg] = useState("");
   const [latLong, setLatLong] = useState("");
+  const [isFindingLocation, setIsFindingLocation] = useState(false);
 
   const success = (position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     setLatLong(`${latitude},${longitude}`);
     setLocationErrorMsg("");
+    setIsFindingLocation(false);
   };
 
   const error = () => {
     setLocationErrorMsg("Unable to retrieve your location");
+    setIsFindingLocation(false);
   };
 
   const handleTrackLocation = () => {
+    setIsFindingLocation(true);
     if (!navigator.geolocation) {
       setLocationErrorMsg("Geolocation is not supported by your browser");
+      setIsFindingLocation(false);
     } else {
       // status.textContent = "Locating…";
       navigator.geolocation.getCurrentPosition(success, error);
     }
   };
 
-  return { locationErrorMsg, latLong, handleTrackLocation };
+  return {
+    locationErrorMsg,
+    latLong,
+    handleTrackLocation,
+    isFindingLocation,
+  };
 };
 
 export default useTrackLocation;
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "@components/styles/Home.module.css";
 import Banner from "@components/components/Banner";
 import Card from "../components/Card";
 import { fetchCoffeeStores } from "../lib/coffee-stores";
+import useTrackLocation from "../hooks/use-track-location";
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores();
@@ -14,9 +15,13 @@ export async function getStaticProps(context) {
 }
 
 export default function Home(props) {
+  const { handleTrackLocation, locationErrorMsg, latLong, isFindingLocation } =
+    useTrackLocation();
+  console.log({ latLong, locationErrorMsg });
+
   const handleOnBannerBtnClick = () => {
     console.log("Banner btn clicked");
-    // handleTrackLocation();
+    handleTrackLocation();
   };
 
   return (
@@ -32,9 +37,11 @@ export default function Home(props) {
       </Head>
       <main className={styles.main}>
         <Banner
-          buttonText="View stores nearby"
+          buttonText={isFindingLocation ? "Locating..." : "View stores nearby"}
           handleOnClick={handleOnBannerBtnClick}
         />
+        {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
+
         <div className={styles.heroImage}>
           <Image
             src={"/static/hero-image.png"}
@@ -43,26 +50,28 @@ export default function Home(props) {
             alt="Coffee brake image"
           />
         </div>
-        {props.coffeeStores.length > 0 && (
-          <>
-            <h2 className={styles.heading2}>Toronto Stores</h2>
-            <div className={styles.cardLayout}>
-              {props.coffeeStores.map((store) => (
-                <Card
-                  id={store.id}
-                  key={store.id}
-                  name={store.name}
-                  ImgUrl={
-                    store.imgUrl ||
-                    "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-                  }
-                  href={`/coffee-store/${store.id}`}
-                  className={styles.card}
-                />
-              ))}
-            </div>
-          </>
-        )}
+        <div className={styles.sectionWrapper}>
+          {props.coffeeStores.length > 0 && (
+            <>
+              <h2 className={styles.heading2}>Toronto Stores</h2>
+              <div className={styles.cardLayout}>
+                {props.coffeeStores.map((store) => (
+                  <Card
+                    id={store.id}
+                    key={store.id}
+                    name={store.name}
+                    ImgUrl={
+                      store.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    href={`/coffee-store/${store.id}`}
+                    className={styles.card}
+                  />
+                ))}
+              </div>
+            </>
+          )}
+        </div>
       </main>
     </>
   );
