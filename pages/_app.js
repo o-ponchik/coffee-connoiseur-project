ed2de88f0f43e3f4e8913ba4073571c4655bb35d
Maria Matviian <matviyanm@gmail.com>
2023-04-05 10:04:27 -0400

    Create Store Reducer in Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,37 @@
 import "@components/styles/globals.css";
 import Layout from "@components/components/layout";
 import { Open_Sans } from "next/font/google";
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const openSans = Open_Sans({ subsets: ["latin"], weight: "400" });
 
 const StoreContext = createContext();
 
+export const ACTION_TYPES = {
+  SET_LAT_LONG: "SET_LAT_LONG",
+  SET_COFFEE_STORES: "SET_COFFEE_STORES",
+};
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return { ...state, latLong: action.payload.latLong };
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return { ...state, coffeeStores: action.payload.latLong };
+    }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
 const StoreProvider = ({ children }) => {
   const initialState = { latLong: "", coffeeStores: [] };
 
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+
   return (
-    <StoreContext.Provider value={{ state: initialState }}>
+    <StoreContext.Provider value={{ state, dispatch }}>
       {children}
     </StoreContext.Provider>
   );
