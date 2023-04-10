39feb05e93ca75a316f52918009a97fe3df63006
Maria Matviian <matviyanm@gmail.com>
2023-04-10 11:09:57 -0400

    create favoriteCoffeeStoreById API

---
+++ pages/api/favoriteCoffeeStoreById.js
@@ -0,0 +1,27 @@
+import { findRecordById } from "../../lib/airtable";
+
+const favoriteCoffeeStoreById = async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const { id } = req.body;
+
+      if (id) {
+        const records = await findRecordById(id);
+
+        if (records.length !== 0) {
+          res.json(records);
+        } else {
+          res.json({ message: "Coffee store id doesn't exist", id });
+        }
+      } else {
+        res.status(400);
+        res.json({ message: "Id is missing" });
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({ message: "Error upvoting coffee store", error });
+    }
+  }
+};
+
+export default favoriteCoffeeStoreById;
