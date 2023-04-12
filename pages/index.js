c5810c54e9979a207e2cac83bd00b8b6682d7314
Maria Matviian <matviyanm@gmail.com>
2023-04-12 16:24:35 -0400

    define radius for getting store by location

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home(props) {
     }
 
     setCoffeeStoresByLocation();
-  }, [latLong]);
+  }, [latLong, dispatch]);
 
   const handleOnBannerBtnClick = () => {
     handleTrackLocation();
